45eda6d390a7f340fe34367654bade8355d88e19
Ross Edlin <rossedlin@gmail.com>
2022-09-12 12:20:34 +0100

    Add Sentry

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/**
+ * Sentry
+ */
+import * as Sentry from '@sentry/react';
+import {BrowserTracing} from '@sentry/tracing';
+
+Sentry.init({
+  dsn:              process.env.REACT_APP_SENTRY_DSN,
+  integrations:     [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
+/**
+ * App
+ */
+import App from './App';
+
+/**
+ * Root
+ */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App/>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

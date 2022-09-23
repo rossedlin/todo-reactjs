cb5c921c0a63915c327f0817b9ae6930dac94bf5
Ross Edlin <rossedlin@gmail.com>
2022-09-23 14:19:10 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,172 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import {FaPlus, FaTrashAlt, FaCheck} from 'react-icons/fa';
+import {getFirestore, collection, getDocs} from 'firebase/firestore/lite';
 
-function App() {
+/**
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function App({firebaseApp}) {
+  const [tasks, setTasks] = useState([]);
+
+  /**
+   *
+   * @param newTask
+   */
+  const onAddTask = (newTask) => {
+    setTasks(currentState => [...currentState, newTask]);
+  };
+
+  /**
+   *
+   * @param taskId
+   */
+  const onDoneTask = (taskId) => {
+    setTasks(currentState => currentState.filter(task => task.id !== taskId));
+  };
+
+  /**
+   *
+   * @returns {(function(): Promise<DocumentData[]|undefined>)|*}
+   */
+  const getTasks = async function() {
+
+    let t      = [];
+    const db   = getFirestore(firebaseApp);
+    const col  = collection(db, 'tasks');
+    const snap = await getDocs(col);
+    const list = snap.docs.map(doc => doc.data());
+
+    let i = 0;
+    await list.forEach((doc) => {
+      t[i++] = {
+        id:          doc.id,
+        name:        doc.data().name,
+      };
+    });
+
+    // setTasks(t);
+
+    return t;
+  };
+
+  useEffect(() => {
+
+    getTasks();
+
+    // getTasks().then((res) => {
+    //   console.log(res);
+    // });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        <h1>Todo ReactJS</h1>
+
+        <Form onSubmit={onAddTask}/>
+
+        <hr/>
+
+        <Tasks tasks={tasks}
+               onDoneTask={onDoneTask}/>
+      </div>
     </div>
   );
 }
 
-export default App;
+/**
+ *
+ *
+ * @param onSubmit
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Form({onSubmit}) {
+  const [taskName, setTaskName] = useState('');
+  const [id, setId]             = useState(1);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!!taskName) {
+      const newTask = {
+        id:   id,
+        name: taskName,
+      };
+
+      setId(id + 1);
+
+      onSubmit(newTask);
+      setTaskName('');
+    }
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input className="input"
+             type="text"
+             value={taskName}
+             placeholder="Task name..."
+             onChange={event => setTaskName(event.target.value)}/>
+
+      <button type="submit"
+              disabled={taskName === ''}>
+        <FaPlus size={12}/>
+        Add
+      </button>
+    </form>
+  );
+}
+
+/**
+ *
+ * @param tasks
+ * @param searchTaskName
+ * @param onDoneTask
+ * @param onChangeCompletedTask
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Tasks({tasks, onRemoveTask: onDoneTask, onChangeCompletedTask}) {
+  return (
+    <ul className="tasks">
+      {tasks.map(task => (
+        <Task
+          {...task}
+          key={task.id}
+          onDone={onDoneTask}
+        />
+      ))}
+    </ul>
+  );
+}
+
+/**
+ *
+ * @param id
+ * @param name
+ * @param completed
+ * @param onDone
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Task({id, name, completed, onDone}) {
+  return (
+    <li className="">
+
+      <span className="task__name">
+        #{id}&nbsp;-&nbsp;
+      </span>
+
+      <span className="task__name">
+        {name}
+      </span>
+
+      <button type="button" onClick={() => onDone(id)}>
+        <FaCheck size={16}/>
+      </button>
+    </li>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-/**
- * App
- */
+// App
 import App from './App';
+import './App.css';
 
-/**
- * Sentry
- */
+// Sentry
 import * as Sentry from '@sentry/react';
 import {BrowserTracing} from '@sentry/tracing';
 
+// Firebase
+import {initializeApp} from 'firebase/app';
+// import {getAuth, onAuthStateChanged} from 'firebase/auth';
+
+/**
+ * Sentry Initialise
+ */
 Sentry.init({
   dsn:              process.env.REACT_APP_SENTRY_DSN,
   integrations:     [new BrowserTracing()],
@@ -20,12 +24,37 @@ Sentry.init({
 });
 
 /**
- * Root
+ * Firebase Initialise
+ */
+const firebaseApp = initializeApp({
+  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.REACT_APP_FIREBASE_APP_ID,
+});
+
+// const auth = getAuth();
+// onAuthStateChanged(auth, (user) => {
+//   if (user) {
+//     // User is signed in, see docs for a list of available properties
+//     // https://firebase.google.com/docs/reference/js/firebase.User
+//     const uid = user.uid;
+//     // ...
+//   } else {
+//     // User is signed out
+//     // ...
+//   }
+// });
+
+/**
+ * ReactJS Initialise
  */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <App firebaseApp={firebaseApp}/>
   </React.StrictMode>,
 );
 

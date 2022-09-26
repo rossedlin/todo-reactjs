6e874c590585e2970a4eacb8b2c174f5fb196c03
Ross Edlin <r.edlin@ucl.ac.uk>
2022-09-26 12:19:16 +0100

    Add docker for window

--- docker.sh
+++ docker.sh
@@ -1,2 +1,3 @@
 
 docker run -it -p 3000:3000 -p 9005:9005 -v "${PWD}":/home/node/app rossedlin/nodejs:14 bash
+#docker run -it -p 3000:3000 -p 9005:9005 -v "/c/Ross Edlin/Projects/todo-reactjs:/home/node/app" rossedlin/nodejs:14 bash

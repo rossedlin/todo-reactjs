a3ff03c43886d6dcdf8f5641cabf585e011b60b1
Ross Edlin <rossedlin@gmail.com>
2022-09-30 18:25:22 +0100

    Update build files

---
+++ down.sh
@@ -0,0 +1,2 @@
+
+rm -R node_modules/
--- up.sh
+++ up.sh
@@ -1,2 +1,8 @@
 
-yarn start
+docker compose run web node -v;
+docker compose run web npm -v;
+docker compose run web yarn -v;
+docker compose run web firebase --version;
+
+docker compose run web yarn install;
+docker compose up;
